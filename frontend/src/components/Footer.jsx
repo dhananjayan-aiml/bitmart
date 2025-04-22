8cf23f080fac8605fafcc0fa31d4bc81002712fb
Dhananjayan <dhananprojects@gmail.com>
2025-04-22 18:23:57 +0530

    intial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            Bit Mart is a local e-commerce platform that provides a secure and user-friendly experience for buying and selling products. With a wide range of supported categories and advanced features, Bit Mart is dedicated to offering a seamless shopping experience for both buyers and sellers.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+91 9003367030</li>
+                <li>contact@bitmart.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025@ bitmart.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+    const [latestProducts,setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+        setLatestProducts(products.slice(0,10));
+    },[products])
+
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+          <Title text1={'IN'} text2={'STOCK'} />
+          <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Find the latest products in our store. We have a wide range of products to choose from.
+          </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          latestProducts.map((item,index)=>(
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
